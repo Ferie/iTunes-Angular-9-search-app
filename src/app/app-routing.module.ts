11d2d1287fdc26e9936754ab9377e331642c6dd2
Riccardo Andreatta <ferie150@hotmail.com>
2020-08-04 16:32:59 +0100

    Added route redirect in case of wrong link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
+        pathMatch: 'full',
         loadChildren: () => import('./modules/pages/search/search.module').then((m) => m.SearchModule),
     },
     {
         path: 'history',
+        pathMatch: 'full',
         loadChildren: () => import('./modules/pages/history/history.module').then((m) => m.HistoryModule),
     },
+    {
+        path: '**',
+        redirectTo: '/'
+    }
 ];
 
 @NgModule({
