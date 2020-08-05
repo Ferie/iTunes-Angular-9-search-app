518bcd4605f9c58b02b189969e00e714b7efbcad
Riccardo Andreatta <ferie150@hotmail.com>
2020-08-05 02:14:44 +0100

    Cleaning unnecessary stuff

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -11,20 +11,4 @@ import { ITunesSearchEntry } from 'src/app/interfaces/itunes-search-entry.interf
 })
 export class CardComponent {
     @Input() public result: ITunesSearchEntry;
-
-    constructor(
-        private searchService: SearchService,
-        private router: Router
-    ) { }
-
-    public resizeImage(imageUrl: string): void {
-        console.log(imageUrl);
-        // imageUrl = imageUrl.substr()
-        // https://is5-ssl.mzstatic.com/image/thumb/Music/v4/f7/05/84/f7058482-c27d-8725-490d-1442a9f1db91/source/100x100bb.jpg
-    }
-
-    public goToDetails(): void {
-    // public goToDetails(url: string): void {
-    //     this.router.navigate(['/details', url], { queryParams: { product: url }, queryParamsHandling: 'merge' });
-    }
 }
