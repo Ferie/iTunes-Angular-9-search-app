ff54eb235f4918b1a3cba34d7cec5311191557c9
Riccardo Andreatta <ferie150@hotmail.com>
2020-08-05 01:57:09 +0100

    Added function to handle network errors

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -17,5 +17,21 @@ export class SearchService {
     public getITunesResults(term: string) {
         // console.log('api url', this.apiUrl);
         return this.http.jsonp<ITunesSearchResults>('https://itunes.apple.com/search?term=' + term, 'callback');
+
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred
+            console.error('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong.
+            console.error(
+                `Backend returned code ${error.status}, ` +
+                `body was: ${error.error}`
+            );
+        }
+
+        // Return an observable with a user-facing error message.
+        return throwError('Something went in sending the request. Please try again.');
     }
 }
